4cab014a2448e17381af5bf53f78b98247b95898
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-03-30 23:19:18 +0000

    Add Html in controller

--- src/main/java/com/basiccrud/crudoperation/hello/HelloController.java
+++ src/main/java/com/basiccrud/crudoperation/hello/HelloController.java
@@ -11,5 +11,22 @@ public class HelloController {
 	public String hello() {
 		return "Hello this is from helloController";
 	}
+	
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String helloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<!DOCTYPE html>");
+		sb.append("<html lang=\"en\">");
+		sb.append("<head>");
+		sb.append("<meta charset=\"UTF-8\">");
+		sb.append("<title>My First Webpage</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("<h1>Hello World! from html</h1>");
+		sb.append("</body>");
+		sb.append("</html>");
+		return sb.toString();
+	}
 
 }
