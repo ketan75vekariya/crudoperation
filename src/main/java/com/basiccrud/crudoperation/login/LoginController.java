9e54986897f65fada21e67e848a94558390542f6
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-03-31 02:42:07 +0100

    Looging code added

--- src/main/java/com/basiccrud/crudoperation/login/LoginController.java
+++ src/main/java/com/basiccrud/crudoperation/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.basiccrud.crudoperation.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
+		logger.debug("This is the requested parameter", name);
 		model.put("name", name);
 		return "login";
 	}
