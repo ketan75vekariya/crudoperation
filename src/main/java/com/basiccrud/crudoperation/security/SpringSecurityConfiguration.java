99d57e017a16824855ffda149ff24cc1fdc40221
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-04-02 17:30:39 +0100

    Added multiuser authentication

--- src/main/java/com/basiccrud/crudoperation/security/SpringSecurityConfiguration.java
+++ src/main/java/com/basiccrud/crudoperation/security/SpringSecurityConfiguration.java
@@ -14,15 +14,21 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class SpringSecurityConfiguration {
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
+		UserDetails userDetails1 = createNewUser("user", "User@123");
+		UserDetails userDetails2 = createNewUser("admin", "Admin@123");
+		
+		return new InMemoryUserDetailsManager(userDetails1,userDetails2);
+	}
+	private UserDetails createNewUser(String username, String password) {
 		Function<String, String> passwordEncoder = input-> passwordEncoder().encode(input);
+		
 		UserDetails userDetails = User.builder()
 				.passwordEncoder(passwordEncoder)
-				.username("user")
-				.password("User@123")
+				.username(username)
+				.password(password)
 				.roles("USER","ADMIN")
 				.build();
-		
-		return new InMemoryUserDetailsManager(userDetails);
+		return userDetails;
 	}
 	@Bean
 	public PasswordEncoder passwordEncoder() {
